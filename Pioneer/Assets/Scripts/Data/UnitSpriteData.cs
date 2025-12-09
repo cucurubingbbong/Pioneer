c86fa6d35edb8c0357d895cb2710fd9167a4d5aa
cucurubingbbong <93061270+cucurubingbbong@users.noreply.github.com>
2025-12-10 00:07:31 +0900

    정상화
    맨땅스타트

--- Pioneer/Assets/Scripts/Data/UnitSpriteData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "UnitSpriteData", menuName = "Unit/UnitSpriteData")]
-public class UnitSpriteData : ScriptableObject
-{
-
-    public Sprite unitPortrait = null;
-    public Sprite unitStandSprite = null;
-}
